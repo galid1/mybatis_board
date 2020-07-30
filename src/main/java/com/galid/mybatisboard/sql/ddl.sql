94b2f4c62d12a0c091d9708add46c9c7be6d8bd0
전준엽 <galid1@naver.com>
2020-07-30 20:15:50 +0900

    mod User 테이블 생성 ddl 수정
    - auto increment 오타 수정

--- src/main/java/com/galid/mybatisboard/sql/ddl.sql
+++ src/main/java/com/galid/mybatisboard/sql/ddl.sql
@@ -3,7 +3,7 @@ create database mybatis_board;
 use mybatis_board;
 
 create table user(
-        userId int primary key auto_incremenet,
+        userId int primary key auto_increment,
         name varchar(255),
         authId varchar(255),
         password varchar(255));
