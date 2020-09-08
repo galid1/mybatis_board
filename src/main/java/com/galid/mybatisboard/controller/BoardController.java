9ef9a1ff73dbacf0ed22aa35c797c667004d3dd4
전준엽 <galid1@naver.com>
2020-09-08 13:07:02 +0900

    mod board 페이지 요청 핸들러 추가

--- src/main/java/com/galid/mybatisboard/controller/BoardController.java
+++ src/main/java/com/galid/mybatisboard/controller/BoardController.java
@@ -3,6 +3,7 @@ package com.galid.mybatisboard.controller;
 import com.galid.mybatisboard.service.BoardService;
 import com.galid.mybatisboard.vo.BoardVO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class BoardController {
     private final BoardService boardService;
+
+    @GetMapping("/boards")
+    public String getBoard() {
+        return "boards/board";
+    }
 
     @PostMapping("/boards")
     public void post(@ModelAttribute BoardVO boardVO) {
